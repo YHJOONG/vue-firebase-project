fac264d7a7d79a945eca26afd1ed7c742e4bc1b4
YHJOONG <yhj8372@naver.com>
2022-12-25 23:45:11 +0900

    changes : navigation drawer 만들기

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       color="primary"
       dark
     >
-      <v-app-bar-nav-icon/>
+      <v-app-bar-nav-icon @click="drawer = !drawer"/>
       <v-toolbar-title>Page title</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon to="/about">
@@ -15,6 +15,18 @@
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-navigation-drawer>
     <v-content>
       <router-view/>
     </v-content>
@@ -29,9 +41,10 @@
 
 export default {
   name: 'App',
-
-  data: () => ({
-    //
-  })
+  data () {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>
