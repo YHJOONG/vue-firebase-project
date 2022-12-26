17856a1a27ce9a1a28d6a6eddf2c117c89492aff
YHJOONG <yhj8372@naver.com>
2022-12-26 17:11:48 +0900

    changes : realtime 쓰고 읽기

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyAV7XfUWtX51c8rLoGITJ2SpyhJREjPTnA',
+  authDomain: 'hj-my-app.firebaseapp.com',
+  databaseURL: 'https://hj-my-app-default-rtdb.firebaseio.com',
+  projectId: 'hj-my-app',
+  storageBucket: 'hj-my-app.appspot.com',
+  messagingSenderId: '746112770069',
+  appId: '1:746112770069:web:312f2d098058d57a9a2e1c'
+}
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <v-app-bar-nav-icon @click="drawer = !drawer"/>
       <site-title :title="title"/>
       <v-spacer></v-spacer>
+      <v-btn icon @click="save"><v-icon>mdi-check</v-icon></v-btn>
+      <v-btn icon @click="read"><v-icon>mdi-numeric</v-icon></v-btn>
       <v-btn icon to="/about">
         <v-icon>mdi-heart</v-icon>
       </v-btn>
@@ -18,9 +16,9 @@
     <v-navigation-drawer app v-model="drawer">
       <site-menu/>
     </v-navigation-drawer>
-    <v-content>
+    <v-main>
       <router-view/>
-    </v-content>
+    </v-main>
     <SiteFooter :footer="footer"/>
   </v-app>
 </template>
@@ -29,6 +27,8 @@
 import SiteTitle from '@/views/site/TitleView'
 import SiteFooter from '@/views/site/FooterView'
 import SiteMenu from '@/views/site/MenuView'
+import { getDatabase, ref, set, onValue } from 'firebase/database'
+
 export default {
   components: { SiteTitle, SiteFooter, SiteMenu },
   name: 'App',
@@ -37,6 +37,27 @@ export default {
       drawer: false,
       title: '나의 타이틀입니다.',
       footer: '푸터입니다.'
+    }
+  },
+  mounted () {
+    console.log(this)
+  },
+  methods: {
+    save () {
+      console.log('save')
+      const db = getDatabase()
+      set(ref(db, 'title/'), {
+        title: '제목입니다.',
+        text: '테스트 텍스트입니다.'
+      })
+    },
+    read () {
+      const db = getDatabase()
+      const starCountRef = ref(db, 'title/')
+      onValue(starCountRef, (snapshot) => {
+        const data = snapshot.val()
+        console.log(data)
+      })
     }
   }
 }
